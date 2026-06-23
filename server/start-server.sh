5cdca1f6570b3f77595726788be83e2935ea12e2
shellaia <dev.shellaia@gmail.com>
2026-06-23 08:06:51 -0500

    chore: align runtime config with local renderer

--- server/start-server.sh
+++ server/start-server.sh
@@ -2,7 +2,7 @@
 
 # PlantUML JAR file
 # PLANT_UML_JAR="plantuml-mit-1.2025.2.jar"
-PLANT_UML_JAR="plantuml-1.2026.2.jar"
+PLANT_UML_JAR="plantuml-1.2026.6.jar"
 
 # Set the port for the PlantUML server
 PLANTUML_PORT=9090
